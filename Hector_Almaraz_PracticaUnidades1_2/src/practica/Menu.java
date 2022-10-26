37ff9307915f0e19133cf34c50aca9af27a5ce24
hmar321 <99360858+hmar321@users.noreply.github.com>
2022-10-26 20:57:13 +0200

    26/10/2022

--- Hector_Almaraz_PracticaUnidades1_2/src/practica/Menu.java
+++ Hector_Almaraz_PracticaUnidades1_2/src/practica/Menu.java
@@ -1,26 +1,16 @@
 package practica;
 
 import javafx.application.Application;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 import practica.controlador.MenuController;
-import practica.modelo.Veterinario;
 
 public class Menu extends Application {
 	private BorderPane rootLayout;
-	// Listado de personas de la aplicación
-	private ObservableList<Veterinario> equipoData = FXCollections.observableArrayList();
 
-	public Menu() {
-		equipoData.add(new Veterinario("John Doe", "Veterinario","john@correo.com"));
-		equipoData.add(new Veterinario("Liz Fernandez", "Peluquería Canina","liz@correo.com"));
-		equipoData.add(new Veterinario("María", "Veterinaria","maria@correo.com"));
-		equipoData.add(new Veterinario("Pepe Perez", "Auxiliar Técnico Veterinario","pepe@correo.com"));
-	}
+	// Listado de personas de la aplicación
 	@Override
 	public void start(Stage primaryStage) {
 		try {
@@ -30,9 +20,8 @@ public class Menu extends Application {
 			rootLayout = (BorderPane) loader.load();
 
 			// Pasamos al controlador de menu el objeto con el BorderPane principal
-			//MenuController menuController = loader.getController();
-			//menuController.setRootLayout(rootLayout);
-			//menuController.setMenuApp(this);
+			MenuController menuController = loader.getController();
+			menuController.setRootLayout(rootLayout);
 
 			// Mostramos la escena del BorderPane de la variable rootLayot
 			Scene scene = new Scene(rootLayout);
@@ -46,13 +35,6 @@ public class Menu extends Application {
 
 	public static void main(String[] args) {
 		launch(args);
-	}
-	/**
-	 * Método get para lista de Veterinario
-	 * @return
-	 */
-	public ObservableList<Veterinario> getVeterinarioData() {
-		return equipoData;
 	}
 
 }
--- Hector_Almaraz_PracticaUnidades1_2/src/practica/controlador/MenuController.java
+++ Hector_Almaraz_PracticaUnidades1_2/src/practica/controlador/MenuController.java
@@ -7,6 +7,8 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -14,17 +16,17 @@ import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TreeItem;
 import javafx.scene.control.TreeView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.GridPane;
-import practica.Menu;
+import javafx.scene.layout.HBox;
 import practica.modelo.Veterinario;
 
 public class MenuController {
 
 	@FXML
-    private TreeView<String> especiesTreeView;
+	private TreeView<String> especiesTreeView;
 	@FXML
 	private TableView<Veterinario> equipoTableView;
 	@FXML
@@ -34,48 +36,89 @@ public class MenuController {
 	@FXML
 	private TableColumn<Veterinario, String> correoCol;
 
-	private Menu menuApp;
 	// Pantalla principal en la que se añade o quita contenido
 	private BorderPane rootLayout;
+	// Lista auxiliar para TableView
+	private ObservableList<Veterinario> data = FXCollections.observableArrayList(
+			new Veterinario("John Doe", "Veterinario", "john@correo.com"),
+			new Veterinario("Liz Fernandez", "Peluquería Canina", "liz@correo.com"),
+			new Veterinario("María García", "Veterinaria", "maria@correo.com"),
+			new Veterinario("Pepe Perez", "Auxiliar Técnico Veterinario", "pepe@correo.com"));
 
 	@FXML
 	private void initialize() {
-		
+
+		// Asociamos cada columna del TableView a una propiedad de la clase Person
+		nombreCol.setCellValueFactory(new PropertyValueFactory<Veterinario, String>("nombre"));
+		especializacionCol.setCellValueFactory(new PropertyValueFactory<Veterinario, String>("especializacion"));
+		correoCol.setCellValueFactory(new PropertyValueFactory<Veterinario, String>("correo"));
+		// Se rellena la tabla con objetos de la clase Person
+		equipoTableView.setItems(data);
+		// imagen treeview
+
 		FileInputStream fis = null;
-    	try {
-			fis = new FileInputStream("images/huellasTree.jpg");
+		try {
+			fis = new FileInputStream("src/practica/images/huellasTree.jpg");
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		}
 
-    	// Icono para el TreeView
-    	ImageView imageView = new ImageView(new Image(fis)); 
-    	imageView.setPreserveRatio(true);
-    	imageView.setFitHeight(20);
-	    	
-    	// TreeView con icono y 4 ítems
-    	TreeItem<String> rootItem = new TreeItem<String> ("Especies", imageView);
-    	rootItem.setExpanded(true);
-    	
-    	List<String> treeListItems = new ArrayList<String>(Arrays.asList("Perro", 
-    			"Gato", "Ave", "Pez","Otro"));
-    	
-    	// Nodos del TreeView
-        for (String item : treeListItems) {
-            TreeItem<String> treeItem = new TreeItem<String>(item);            
-            rootItem.getChildren().add(treeItem);
-        }     
-        
-        especiesTreeView.setRoot(rootItem);
+		// Icono para el TreeView
+		ImageView imageView = new ImageView(new Image(fis));
+		imageView.setPreserveRatio(true);
+		imageView.setFitHeight(20);
+
+		// TreeView con icono
+		TreeItem<String> rootItem = new TreeItem<String>("Especies", imageView);
+		rootItem.setExpanded(true);
+
+		List<String> treeListItems = new ArrayList<String>(Arrays.asList("Perro", "Gato", "Ave", "Pez", "Otro"));
+
+		// Nodos del TreeView
+		for (String item : treeListItems) {
+			TreeItem<String> treeItem = new TreeItem<String>(item);
+			rootItem.getChildren().add(treeItem);
+		}
+
+		especiesTreeView.setRoot(rootItem);
 	}
 
+	@FXML
+	private void abrirMenu(ActionEvent event) {
+		try {
+			// Cargamos el archivo Controles Dinámicos
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(MenuController.class.getResource("/practica/vista/Menu.fxml"));
+			BorderPane listadoControles = (BorderPane) loader.load();
+			
+			// Se sitúa en el centro del diseño principal
+			rootLayout.setCenter(listadoControles);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
 	@FXML
 	private void abrirFormulario(ActionEvent event) {
 		try {
 			// Cargamos el archivo Controles Dinámicos
 			FXMLLoader loader = new FXMLLoader();
-			loader.setLocation(MenuController.class.getResource("/controlador/Formulario.fxml"));
-			GridPane listadoControles = (GridPane) loader.load();
+			loader.setLocation(MenuController.class.getResource("/practica/vista/Formulario.fxml"));
+			BorderPane listadoControles = (BorderPane) loader.load();
+
+			// Se sitúa en el centro del diseño principal
+			rootLayout.setCenter(listadoControles);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	@FXML
+	private void abrirTutorial(ActionEvent event) {
+		try {
+			// Cargamos el archivo Controles Dinámicos
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(MenuController.class.getResource("/practica/vista/Tutorial.fxml"));
+			HBox listadoControles = (HBox) loader.load();
 
 			// Se sitúa en el centro del diseño principal
 			rootLayout.setCenter(listadoControles);
@@ -96,13 +139,6 @@ public class MenuController {
 
 	public void setRootLayout(BorderPane rootLayout) {
 		this.rootLayout = rootLayout;
-	}
-
-	public void setMenuApp(Menu menuApp) {
-		this.menuApp = menuApp;
-
-		equipoTableView.setItems(this.menuApp.getVeterinarioData());
-
 	}
 
 }
